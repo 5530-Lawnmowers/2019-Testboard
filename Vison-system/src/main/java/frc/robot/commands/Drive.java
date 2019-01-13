c29d76430726944f996eb469727a16157846ce36
Ryan <rkersten@students.greenhillsschool.org>
2019-01-13 10:45:02 -0500

    Basic drive forward code

--- Vison-system/src/main/java/frc/robot/commands/Drive.java
+++ Vison-system/src/main/java/frc/robot/commands/Drive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,25 +8,28 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
-
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+import frc.robot.*;
+public class Drive extends Command {
+  public double Rspeed = .01;
+  public double Lspeed = .01;
+  public Drive() {
     // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    super("Drive");
     requires(Robot.m_subsystem);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+   
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.m_subsystem.setSpeed(Rspeed, Lspeed);
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
--- Vison-system/src/main/java/frc/robot/subsystems/drivetrain.java
+++ Vison-system/src/main/java/frc/robot/subsystems/drivetrain.java
@@ -7,18 +7,37 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.*;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class ExampleSubsystem extends Subsystem {
+public class drivetrain extends Subsystem {
+  TalonSRX Leftup = new TalonSRX(0);
+  TalonSRX Leftback = new TalonSRX(0);
+  TalonSRX Rightup = new TalonSRX(0);
+  TalonSRX Rightdown = new TalonSRX(0);
+    
+    
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  public void setSpeed(double Rspeed, double Lspeed){
+    Rightdown.set(ControlMode.PercentOutput, Rspeed);
+    Rightup.set(ControlMode.PercentOutput, Rspeed);
+    Leftback.set(ControlMode.PercentOutput, Lspeed);
+    Leftup.set(ControlMode.PercentOutput, Lspeed);
+    
 
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new Drive());
   }
 }
