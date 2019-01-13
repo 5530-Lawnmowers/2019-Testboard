3a7214342af0ea901962007a434ac52201f91420
Drive-computer <lawrencebitzer@gmail.com>
2019-01-13 10:52:18 -0500

    Basic code for brushless motor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANDigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.networktables.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,10 +26,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  public  CANSparkMax motor  = new CANSparkMax(2, MotorType.kBrushless);
+  private ShuffleboardTab speedTab = Shuffleboard.getTab("Speed");
+  SimpleWidget motorSpeedWidget;
+  public CANDigitalInput digitalInput;
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -30,9 +39,9 @@ public class Robot extends IterativeRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    motorSpeedWidget = speedTab.add("motorSpeed", 0);
+    
   }
 
   /**
@@ -47,23 +56,8 @@ public class Robot extends IterativeRobot {
   public void robotPeriodic() {
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector",
-    // defaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   /**
@@ -71,22 +65,26 @@ public class Robot extends IterativeRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
   }
 
   /**
    * This function is called periodically during operator control.
    */
+
+  @Override
+  public void teleopInit(){
+    final NetworkTableEntry motorSpeed = motorSpeedWidget.getEntry();
+    motor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    motor.set(motorSpeed.getDouble(1.0));
+    digitalInput = new CANDigitalInput(motor, CANDigitalInput.LimitSwitch.kReverse, CANDigitalInput.LimitSwitchPolarity.kNormallyClosed);
+
+
+  }
+
   @Override
   public void teleopPeriodic() {
+    System.out.println(digitalInput.get());
+
   }
 
   /**
